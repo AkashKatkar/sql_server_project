e91d1a08758e1e9e7bafb5b310ecdf3906763588
Akash Katkar <katkar.akash184@gmail.com>
2022-11-20 23:12:11 +0530

    Add css file & update old file

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>SQL Server</title>
+        <meta name="description" content="SQL server practice">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            table, th, td{
+                border: 1px solid #000;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Country Code</th>
+                    <th>Contact Number</th>
+                </tr><?php
+                include 'connect_db.php';
+                $a = 1;
+                $stmt = sqlsrv_query($conn, 'SELECT * FROM test_tbl');
+                while($rows = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){ ?>
+                    <tr>
+                        <td><?php echo $a; ?></td>
+                        <td><?php echo $rows['username']; ?></td>
+                        <td><?php echo $rows['country_code']; ?></td>
+                        <td><?php echo $rows['pnum']; ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </body>
+</html>

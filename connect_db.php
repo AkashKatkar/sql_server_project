7047ea9d401dadb95e76f5e146cef87dd5371dc1
Akash Katkar <katkar.akash184@gmail.com>
2022-11-20 21:09:26 +0530

    first commit

---
+++ connect_db.php
@@ -0,0 +1,7 @@
+<?php
+    $serverName = "LAPTOP-HPL2O8ER\SQLEXPRESS";
+    $connectionInfo = array( "Database"=>"test_db", "UID"=>"LAPTOP-HPL2O8ER\katka", "PWD"=>"Akash@#$");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    $query = sqlsrv_query();
+?>
